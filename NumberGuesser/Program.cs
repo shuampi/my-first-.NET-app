115207990debc7a4ced1f656dda655721811bc8d
shuamp <castel.davila@gmail.com>
2022-06-30 14:26:08 +0100

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program 
+    { 
+    static void Main (string[] args)
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Juan Castel"; 
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            Console.ResetColor();
+
+            Console.WriteLine("What is your name?");
+
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello {0}, let's play a game!", input);
+
+
+        }
+    }
+}
+
+
+
